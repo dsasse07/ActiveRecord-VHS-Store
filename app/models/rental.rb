d441f325a59b6c067c850b28c238133d2b429ffe
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-12-23 12:49:24 -0500

    up to Rental past_due_date

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,16 @@
 class Rental < ActiveRecord::Base
     belongs_to :client
     belongs_to :vhs
+
+    def due_date
+        # binding.pry
+        self.created_at + 7.days
+    end
+
+    def self.past_due_date
+        self.all.select do |rental| 
+            (rental.current == true && rental.due_date < DateTime.now) || (rental.current == false && rental.due_date < rental.updated_at)
+        end
+        binding.pry
+    end
 end
