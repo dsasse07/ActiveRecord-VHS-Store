ec1f296012bd26fe750f59701dc6bc32c01256cd
Daniel Sasse <dsasse07@gmail.com>
2020-12-23 11:14:06 -0500

    Movies migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_23_161052) do
+ActiveRecord::Schema.define(version: 2020_12_23_161219) do
 
   create_table "clients", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 2020_12_23_161052) do
   create_table "movie_genres", force: :cascade do |t|
     t.integer "movie_id"
     t.integer "genre_id"
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "year"
+    t.integer "length"
+    t.string "director"
+    t.string "description"
+    t.boolean "female_director"
   end
 
   create_table "rentals", force: :cascade do |t|
