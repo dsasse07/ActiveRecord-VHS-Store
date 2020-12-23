b36908abda2ee0e0579e747d0b82d6d8b47f8922
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-12-23 15:57:54 -0500

    Client.most_active finished

--- app/models/client.rb
+++ app/models/client.rb
@@ -12,4 +12,15 @@ class Client < ActiveRecord::Base
         available_copy = vhs_copies.select {|copy| copy.is_available_to_rent? || !copy.ever_rented? }.first
         Rental.create(vhs_id: available_copy.id , client_id: new_client.id, current: true)
     end
+
+    def num_of_rentals
+        Rental.where(client_id: self.id, current: false).count
+    end
+
+    def self.most_active
+        #get all of the clients
+        #get a count of how many rental instances have their client_id
+        #order them by highest number of rentals returned, and then return the top 5.
+        self.all.each_with_object({}) { |client, hash| hash[client] = client.num_of_rentals }.sort_by(&:last).pop(5)
+    end
 end
