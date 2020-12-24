240bd1f441f75df7713510aed48970855af3a041
Daniel Sasse <dsasse07@gmail.com>
2020-12-24 14:28:22 -0500

    Movie.most_rentals Done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -16,4 +16,13 @@ class Movie < ActiveRecord::Base
         movies_hash = self.all.each_with_object({}) { |movie, movie_hash| movie_hash[movie] = movie.movie_clients.uniq.count }
         movies_hash.max_by {|movie, client_count| client_count}[0]
     end
+
+    def self.most_rentals
+        vhs_hash = Vhs.count_vhs_by_client
+        movie_by_client_count = vhs_hash.each_with_object({}) do |(vhs, times_rented), movie_hash|
+                movie_hash[vhs.movie].nil? ? movie_hash[vhs.movie] = times_rented : movie_hash[vhs.movie] += times_rented
+        end
+        movie_by_client_count.sort_by(&:last).pop(3).to_h.keys
+    end
+    
 end
